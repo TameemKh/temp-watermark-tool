8851bc1e34722348fe9094afe5114e1fc6672a28
TameemKh <tameemkhanfar@gmail.com>
2026-05-22 01:21:15 +0300

    Update watermark.py

--- pages/watermark.py
+++ pages/watermark.py
@@ -375,23 +375,23 @@ with preview_col:
             # =========================
             # PAGE SELECTOR
             # =========================
+            with controls_col:
+                st.markdown("### اختيار الصفحة")
 
-            st.markdown("### اختيار الصفحة")
-
-            preview_page_number = st.slider(
-                "التنقل السريع",
-                min_value=1,
-                max_value=total_pages,
-                value=1
-            )
-
-            preview_page_number = st.number_input(
-                "أو اكتب رقم الصفحة",
-                min_value=1,
-                max_value=total_pages,
-                value=preview_page_number,
-                step=1
-            )
+                preview_page_number = st.slider(
+                    "التنقل السريع",
+                    min_value=1,
+                    max_value=total_pages,
+                    value=1
+                )
+            with controls_col:
+                preview_page_number = st.number_input(
+                    "أو اكتب رقم الصفحة",
+                    min_value=1,
+                    max_value=total_pages,
+                    value=preview_page_number,
+                    step=1
+                )
 
             # =========================
             # GET PAGE
